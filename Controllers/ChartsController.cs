b9c70e14e41102c812656a7d24eaf58c157c99b6
Lucas-Sholtz <lucas.sholtz@gmail.com>
2021-03-15 18:08:04 +0200

    1.5

---
+++ Controllers/ChartsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace OrdersSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChartsController : ControllerBase
+    {
+        private readonly OrderSystemDatabaseContext _context;
+        public ChartsController(OrderSystemDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("Couriers")]
+        public JsonResult Couriers()
+        {
+            var couriers = _context.Couriers.Include(c => c.Orders).ToList();
+            //var table = new List<Tuple<string, int>>();
+            List<object> list = new List<object>();
+            list.Add(new [] { "Сourier", "Order count" });
+            foreach(var c in couriers)
+            {
+                list.Add(new object[] { c.CourierName, c.Orders.Count() });
+                //table.Add(new Tuple<string, int>(c.CourierName, c.Orders.Count));
+            }
+
+            return new JsonResult(list);
+        }
+
+        [HttpGet("Shops")]
+        public JsonResult Shops()
+        {
+            var shops = _context.Shops.Include(c => c.Products).ToList();
+            //var table = new List<Tuple<string, int>>();
+            List<object> list = new List<object>();
+            list.Add(new[] { "Shop", "Products count" });
+            foreach (var c in shops)
+            {
+                list.Add(new object[] { c.ShopName, c.Products.Count() });
+                //table.Add(new Tuple<string, int>(c.CourierName, c.Orders.Count));
+            }
+
+            return new JsonResult(list);
+        }
+    }
+}
